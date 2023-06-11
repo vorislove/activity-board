ea04675a69beaf427e7b87dff63ebed4305a1e9e
Vlad Gareev <gar.vlad.ix@gmail.com>
2023-06-11 19:33:45 +0400

    изменил конфиг

--- src/shared/config/firebase.ts
+++ src/shared/config/firebase.ts
@@ -4,13 +4,16 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-	apiKey: process.env.REACT_APP_API_KEY,
-	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-	projectId: process.env.REACT_APP_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-	appId: process.env.REACT_APP_APP_ID
+	apiKey: process.env.VITE_API_KEY,
+	authDomain: process.env.VITE_AUTH_DOMAIN,
+	projectId: process.env.VITE_PROJECT_ID,
+	storageBucket: process.env.VITE_STORAGE_BUCKET,
+	messagingSenderId: process.env.VITE_MESSAGING_SENDER_ID,
+	appId: process.env.VITE_APP_ID
 };
+
+const i = process.env.VITE_APP_ID;
+console.log(i);
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
