c56be229701de093b84163595854218812750a29
Vlad Gareev <gar.vlad.ix@gmail.com>
2023-06-12 13:53:30 +0400

    fix 2

--- src/shared/ui/index.tsx
+++ src/shared/ui/index.tsx
@@ -1,6 +1,6 @@
-export * from './card/Card';
-export * from './icon/Icon';
-export * from './loader/Loader';
+export { Card } from './card/Card';
+export { Icon } from './icon/Icon';
+export { Loader } from './loader/Loader';
 export { Avatar } from './avatar/Avatar';
 export { Button } from './button/Button';
 export { Skeleton } from './skeleton/Skeleton';
