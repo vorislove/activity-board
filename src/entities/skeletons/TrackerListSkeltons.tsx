9085b4c526a0482e9e00c7c41a5e9f849bf2210f
Vlad Gareev <gar.vlad.ix@gmail.com>
2023-07-23 00:18:33 +0400

    fix skeletons in desktop

--- src/entities/skeletons/TrackerListSkeltons.tsx
+++ src/entities/skeletons/TrackerListSkeltons.tsx
@@ -5,11 +5,12 @@ export const TrackerListSkeltons = () => {
 	const skeletons = new Array(6).fill(null);
 	const typeDevice = useDeviceType();
 	const height = typeDevice === 'mobile' ? '159px' : '186px';
+	const width = typeDevice === 'mobile' ? '100%' : '250px';
 
 	return (
 		<>
 			{skeletons.map((_, index) => (
-				<Skeleton height={height} key={index} />
+				<Skeleton height={height} key={index} width={width} />
 			))}
 		</>
 	);
